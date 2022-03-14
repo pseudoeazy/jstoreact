5fc887a2204c75b9ed78bf8fa8ebac9ede81028b
Israel <israel4sng@yahoo.com>
2022-03-14 16:14:23 +0800

    remove unecessary files

--- meal-store-react/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import View from "./components/View.jsx";
-
-const storeSection = document.getElementById("storeSection");
-ReactDOM.render(<View />, storeSection);
